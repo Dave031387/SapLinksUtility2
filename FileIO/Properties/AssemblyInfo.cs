eafecd8136732760f208142c4cca200b3e78dd31
dave031387@outlook.com <dave031387@outlook.com>
2018-07-21 22:42:32 -0400

    Build 7/21/2018

--- FileIO/Properties/AssemblyInfo.cs
+++ FileIO/Properties/AssemblyInfo.cs
@@ -25,5 +25,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers by using the '*'
 // as shown below: [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2018.7.20.0218")]
+[assembly: AssemblyVersion("2018.7.22.0125")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
